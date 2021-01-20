417bef5f73cb43a643edb969637d8b6a30d70a89
damatoaj <mr.arthurdamato@gmail.com>
2021-01-20 11:34:50 -0800

    8 passing in the test

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcrypt');
 const {
   Model
 } = require('sequelize');
@@ -14,10 +15,49 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   user.init({
-    email: DataTypes.STRING,
-    name: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email address'
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 99],
+          msg: 'Name must be between 1 and 99 characters'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8,99],
+          msg: 'Password must be at least 9 characters'
+        },
+        notContains: {
+          args: this.name,
+          msg: 'Password cannot contain your name'
+        }
+      }
+    }
+  }, 
+    {
+    hooks: {
+      beforeCreate: (pendingUser, options) => {
+        //check if there is a user being passed AND that that user has a password
+        if(pendingUser && pendingUser.password) {
+          //hash the password
+          let hash = bcrypt.hashSync(pendingUser.password, 12);
+          //store the hash as the user's password
+          pendingUser.password = hash
+        }
+      }
+    },
     sequelize,
     modelName: 'user',
   });
